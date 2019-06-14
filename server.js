28336b23ca70618f3905ec574918b16f006d1a06
Jeanine Hoffman <jeanine.hoffman1970@gmail.com>
2019-06-14 14:52:12 -0700

    trouble with api fetch but I think the code is mostly good

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
-
-
-const MOVIEDEX = require('./moviedex.json')
+const helmet = require('helmet')
+const cors = require('cors')
+const MOVIEs = require('./movie-data-small.json')
 
 const app = express()
 
 app.use(morgan('dev'))
+app.use(cors())
+app.use(helmet())
+
 
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN
   const authToken = req.get('Authorization')
+  console.log(authToken)
+  console.log(apiToken)
   if (!authToken || authToken.split(' ')[1] !== apiToken) {
     return res.status(401).json({ error: 'Unauthorized request' })
   }
@@ -20,26 +25,36 @@ app.use(function validateBearerToken(req, res, next) {
 })
 const validGenre = ['Animation', 'Drama', 'Romantic', 'Comedy', 'Spy', 'Crime', 'Thriller', 'Adventure', 'Documentary', 'Horror', 'Action', 'Western', 'History', 'Biography', 'Musical', 'Fantasy', 'War', 'Grotesque']
 
-app.get('/genre', function handleGetGenre(req, res) {
-  res.json(validTGenre)
+app.get('/movies', function handleGetMovies(req, res) {
+  let response = MOVIES.movies;
+
+  if (req.query.avg_vote) {
+    response = response.filter(movie =>
+      Number(movie.avg_vote) >= Number(req.query.avg_votes))
+  }
+
+  if (req.query.country) {
+    response = response.filter(movie =>
+      // case insensitive searching
+      movie.country.toLowerCase().includes(req.query.country.toLowerCase())
+    )
+  }
+
+  if (req.query.genre) {
+    response = response.filter(movie =>
+      // case insensitive searching
+      movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+    )
+  }
+
+  if (response.length === 0) {
+    res.status(200).send('We didn\'t find any movies!');
+  } else {
+    res.json(response);
+  }
 })
 
 
-function handleGetGenre(req, res) {
-
-}
-app.get('/genre', handleGetGenre)
-
-function handleGetFilmTitle(req, res) {
-  res.send('movie time!')
-}
-if (req.query.name) {
-  response = response.filter(genre =>
-    // case insensitive searching
-    genre.name.toLowerCase().includes(req.query.name.toLowerCase())
-  )
-}
-app.get('/title', handleGetFilmTitle)
 
 const PORT = 8000
 
